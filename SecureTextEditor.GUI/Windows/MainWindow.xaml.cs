642f52a176385890f53b0218a1eb9e8da0b3eb08
Thimilius <mail@thimilius.com>
2019-04-30 23:08:35 +0200

    Add drag and drop for tab control and add functional editor status

--- SecureTextEditor.GUI/Windows/MainWindow.xaml.cs
+++ SecureTextEditor.GUI/Windows/MainWindow.xaml.cs
@@ -54,6 +54,54 @@ namespace SecureTextEditor.GUI {
             ChangeEncoding(menuItem.Header.Equals("UTF-8") ? Encoding.UTF8 : Encoding.ASCII, menuItem);
         }
 
+        private void EditorTextChanged(object sender, RoutedEventArgs e) {
+            UpdateEditorStatus(sender);
+        }
+
+        private void EditorSelectionChanged(object sender, RoutedEventArgs e) {
+            UpdateEditorStatus(sender);
+        }
+
+        private void UpdateEditorStatus(object sender) {
+            TextBox editor = sender as TextBox;
+            LinesLabel.Text = $"Lines: {editor.LineCount}";
+            int caretIndex = editor.CaretIndex;
+            int lineIndex = editor.GetLineIndexFromCharacterIndex(caretIndex);
+            LineLabel.Text = $"Ln: {lineIndex + 1}";
+            ColumnLabel.Text = $"Col: {caretIndex - editor.GetCharacterIndexFromLineIndex(lineIndex)}";
+            SelectionLabel.Text = $"Sel: {editor.SelectionLength}";
+        }
+
+        private void TabControlPreviewMouseMove(object sender, MouseEventArgs e) {
+            if (!(e.Source is TabItem tabItem)) {
+                return;
+            }
+
+            if (Mouse.PrimaryDevice.LeftButton == MouseButtonState.Pressed) {
+                DragDrop.DoDragDrop(tabItem, tabItem, DragDropEffects.All);
+            }
+        }
+
+        private void TabControlDrop(object sender, DragEventArgs e) {
+            var tabItemTarget = e.Source as TabItem;
+            var tabItemSource = e.Data.GetData(typeof(TabItem)) as TabItem;
+
+            if (!tabItemTarget.Equals(tabItemSource)) {
+                var tabControl = tabItemTarget.Parent as TabControl;
+                int targetIndex = tabControl.Items.IndexOf(tabItemTarget);
+
+                tabControl.Items.Remove(tabItemSource);
+                tabControl.Items.Insert(targetIndex, tabItemSource);
+
+                tabItemSource.Focus();
+            }
+        }
+
+        private void TabControlGiveFeedback(object sender, GiveFeedbackEventArgs e) {
+            e.UseDefaultCursors = false;
+            e.Handled = true;
+        }
+
         private void ChangeTheme(Theme theme, MenuItem clickedItem) {
             m_Theme = theme;
             Uri locator = theme == Theme.DarkMode ? ResourceLocator.DarkColorScheme : ResourceLocator.LightColorScheme;
