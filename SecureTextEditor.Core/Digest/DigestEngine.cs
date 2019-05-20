d7fb57dc12a0d554610f30bdc6b8db093846a261
Thimilius <mail@thimilius.com>
2019-05-20 19:10:37 +0200

    Add basis for digests

---
+++ SecureTextEditor.Core/Digest/DigestEngine.cs
@@ -0,0 +1,9 @@
+﻿namespace SecureTextEditor.Core.Digest {
+    public class DigestEngine {
+        private DigestType m_Type;
+
+        public DigestEngine(DigestType type) {
+            m_Type = type;
+        }
+    }
+}
---
+++ SecureTextEditor.Core/Digest/DigestType.cs
@@ -0,0 +1,19 @@
+﻿namespace SecureTextEditor.Core.Digest {
+    /// <summary>
+    /// Describes the digest type.
+    /// </summary>
+    public enum DigestType {
+        /// <summary>
+        /// Describes the SHA-256 algorithm.
+        /// </summary>
+        SHA256,
+        /// <summary>
+        /// Describes a MAC using AES with the CMAC algotithm.
+        /// </summary>
+        AESCMAC,
+        /// <summary>
+        /// Describes the Hash MAC with the SHA-256 algotithm.
+        /// </summary>
+        HMACSHA256
+    }
+}
