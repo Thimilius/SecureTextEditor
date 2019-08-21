5591caff0d06c013d4a7e574551dc16136b8f4ed
Thimilius <mail@thimilius.com>
2019-08-21 20:08:12 +0200

    Further implement digest tests

--- SecureTextEditor.Tests/DigestEngingeTester.cs
+++ SecureTextEditor.Tests/DigestEngingeTester.cs
@@ -1,30 +1,46 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Org.BouncyCastle.Utilities.Encoders;
+using SecureTextEditor.Crypto;
 using SecureTextEditor.Crypto.Digest;
 
 namespace SecureTextEditor.Tests {
     [TestClass]
     public class DigestEngingeTester {
         private static readonly byte[] MESSAGE = Hex.Decode("000102030405060708090a0b0c0d0e0f");
+        private static readonly byte[] ALTERED_MESSAGE = Hex.Decode("000102030405060708090a0b0c0d0e00");
         private static readonly byte[] KEY = Hex.Decode("000102030405060708090a0b0c0d0e0f");
+        private static readonly byte[] ALTERED_KEY = Hex.Decode("000102030405060708090a0b0c0d0e00");
 
         [TestMethod]
         public void SHA256_Test() {
             DigestEngine engine = new DigestEngine(DigestType.SHA256);
-            var output = engine.Digest(MESSAGE, null);
-            Assert.AreEqual(output.Length, 32);
+            byte[] digest1 = engine.Digest(MESSAGE, null);
+            byte[] digest2 = engine.Digest(ALTERED_MESSAGE, null);
+            Assert.IsFalse(SecurityExtensions.AreEqual(digest1, digest2));
         }
 
         [TestMethod]
         public void AESCMAC_Test() {
             DigestEngine engine = new DigestEngine(DigestType.AESCMAC);
-            var output = engine.Digest(MESSAGE, KEY);
+            byte[] digest1 = engine.Digest(MESSAGE, KEY);
+            byte[] digest2 = engine.Digest(ALTERED_MESSAGE, KEY);
+            Assert.IsFalse(SecurityExtensions.AreEqual(digest1, digest2));
         }
 
         [TestMethod]
         public void HMACSHA256_Test() {
             DigestEngine engine = new DigestEngine(DigestType.HMACSHA256);
-            var output = engine.Digest(MESSAGE, KEY);
+            byte[] digest1 = engine.Digest(MESSAGE, KEY);
+            byte[] digest2 = engine.Digest(ALTERED_MESSAGE, KEY);
+            Assert.IsFalse(SecurityExtensions.AreEqual(digest1, digest2));
+        }
+
+        [TestMethod]
+        public void Key_Test() {
+            DigestEngine engine = new DigestEngine(DigestType.HMACSHA256);
+            byte[] digest1 = engine.Digest(MESSAGE, KEY);
+            byte[] digest2 = engine.Digest(MESSAGE, ALTERED_KEY);
+            Assert.IsFalse(SecurityExtensions.AreEqual(digest1, digest2));
         }
     }
 }
